145e4af347ccac97fb8593a6f448eeccfdd97d25
rocksnow1942 <rocksnow1942@gmail.com>
2019-10-02 09:21:30 -0700

    t

--- foldojo/main.py
+++ foldojo/main.py
@@ -1,7 +1,16 @@
 from foldojo_module import structure_prediction
 from bokeh.io import curdoc
-predictlayout=structure_prediction().layout
 
+
+from dotenv import load_dotenv
+
+load_dotenv('/home/hui/ngs_server/app/APPS/.env')
+
+
+
+
+
+predictlayout=structure_prediction().layout
 
 curdoc().add_root(predictlayout)
 curdoc().title = chr(128540) + 'Foldojo'
